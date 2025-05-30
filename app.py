8cdda7a141cd05a3fb5168637c242e57d6f70342
Marin Janssen <marin@fedora.fritz.box>
2025-05-30 10:12:15 +0200

    cleaned file app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from card import *
-from deck import *
-from hand import Hand
+from deck import myDeck1
 
 
 def monteCarlo(num_trials, myDeck):
@@ -10,25 +8,17 @@ def monteCarlo(num_trials, myDeck):
     # counter_zero_starter = 0
     # counter_one_starter = 0
     # counter_two_starter = 0
-    while( counter < num_trials):
+    while (counter < num_trials):
         counter += 1
         myHand = myDeck.drawHand()
         matrix[myHand.numberStarter()][myHand.numberNonEngine()] += 1
-        # match myHand.numberStarter():
-        #     case 0:
-        #         counter_zero_starter += 1
-        #     case 1:
-        #         counter_one_starter += 1
-        #     case 2:
-        #         counter_two_starter += 1
     print(matrix)
-    # print(f"Zero Starter: {counter_zero_starter}, OneStarter: {counter_one_starter}, TwoStarter: {counter_two_starter}")
+
 
 def run():
     myDeck = myDeck1()
     monteCarlo(1000000, myDeck)
-    # print(myDeck)
-    
+
 
 if __name__ == "__main__":
     run()
